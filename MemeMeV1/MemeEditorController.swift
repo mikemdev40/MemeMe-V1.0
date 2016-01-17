5a8270b89c37ae8e48f3aa3a661b5fa46d699432
mike miller <mikemdev40@gmail.com>
2016-01-16 22:15:12 -0500

    Add photo library functionality
    User able to pick photos using the "album" key

--- MemeMeV1/MemeEditorController.swift
+++ MemeMeV1/MemeEditorController.swift
@@ -7,26 +7,68 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class MemeEditorController: UIViewController {
+class MemeEditorController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
+    
+    func shareMeme() {
+        
+    }
+    
+    func pickImageFromAlbum() {
+        getImage(.PhotoLibrary)
+    }
+    
+    func takeImageWithCamera() {
+        getImage(.Camera)
+    }
+    
+    func getImage(sourceType: UIImagePickerControllerSourceType) {
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = self
+        picker.allowsEditing = true
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        var image = info[UIImagePickerControllerEditedImage] as? UIImage
+        if image == nil {
+            image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        }
+        imageView.image = image
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         title = "Meme Editor"
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel", style: .Plain, target: nil, action: nil)
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: nil, action: nil)
+    
+        let barSpace = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
+        let cameraButton = UIBarButtonItem(barButtonSystemItem: .Camera, target: self, action: "takeImageWithCamera")
+        let albumButton = UIBarButtonItem(title: "Album", style: .Plain, target: self, action: "pickImageFromAlbum")
+        toolbarItems = [barSpace, cameraButton, barSpace, albumButton, barSpace]
+        
+        if !UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            cameraButton.enabled = false
+        }
+        if !UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+            albumButton.enabled = false
+        }
     }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        
-        let barSpace = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
-        let cameraButton = UIBarButtonItem(barButtonSystemItem: .Camera, target: nil, action: nil)
-        let albumButton = UIBarButtonItem(title: "Album", style: .Plain, target: nil, action: nil)
-        toolbarItems = [barSpace, cameraButton, barSpace, albumButton, barSpace]
+
         navigationController?.setToolbarHidden(false, animated: true)
 
     }
--- MemeMeV1/MemeObject.swift
+++ MemeMeV1/MemeObject.swift
@@ -7,7 +7,11 @@
 //
 
 import Foundation
+import UIKit
 
 struct MemeObject {
-    
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
 }
